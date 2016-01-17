ee764032eedcab24d436a86dd3d6ba62dd6c1e2f
Will Sorenson <ryan.d.sorenson@gmail.com>
2016-01-17 13:57:44 -0500

    Explain config file

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-BLOG_DIR = '/Users/Will/Devel/tec_blog/'
 AUTHOR = 'Will Sorenson'
+# Location of the pelican Blog
+BLOG_DIR = '/Users/Will/Devel/tec_blog/'
+
+# Directory that posted notebooks should be moved to
 POSTED_DIR = '/Users/Will/Devel/blog_posts/posted/'
